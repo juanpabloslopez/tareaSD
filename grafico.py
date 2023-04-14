503b6bb6bf0bd3cc0a3edaf4b3a03593aacd7f0f
juanpabloslopez <jpschiaffino@gmail.com>
2023-04-14 16:45:49 -0400

    comentarios al grafico.py y eliminar referencias

--- grafico.py
+++ grafico.py
@@ -1,12 +1,15 @@
+# Importar matplotlib
 import matplotlib.pyplot as plt
 
-# Leer los tiempos del archivo
+# [Leer los tiempos del archivo
 with open("tiempos.txt", "r") as f:
     tiempos = [float(line.strip()) for line in f]
+# Leer los tiempos del archivo]
 
-# Crear el gráfico
+# [Crear el gráfico
 plt.plot(tiempos)
 plt.xlabel("Número de ejecución")
 plt.ylabel("Tiempo (segundos)")
 plt.title("Tiempos de ejecución")
 plt.show()
+# Crear el gráfico]
