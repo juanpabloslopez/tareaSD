9e7d2c2d45873f7335c97a9cc2e007a8a43f2765
juanpabloslopez <jpschiaffino@gmail.com>
2023-05-19 18:28:02 -0400

    implementation kafka in code of rabbitmq!

--- tarea2/code.py
+++ tarea2/code.py
@@ -14,7 +14,8 @@ RABBITMQ_QUEUE = 'my_queue'
 
 
 # Configuración de Kafka
-# bootstrap_servers = local
+bootstrap_servers = 'localhost:9092'
+topic = 'kafkin'
 
 # Generar palabra aleatoria
 def generar_palabra_aleatoria():
@@ -22,8 +23,14 @@ def generar_palabra_aleatoria():
     palabra = ''.join(random.choices(string.ascii_letters, k=longitud))
     return palabra
 
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'Error al enviar el mensaje: {err}')
+    else:
+        print(f'Mensaje enviado al topic {msg.topic()} - partición {msg.partition()} - offset {msg.offset()}')
+
 # Enviar palabra aleatoria a RabbitMQ
-def enviar_rabbitmq(palabra):
+def enviar_servers(palabra):
     credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
     parameters = pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT, credentials=credentials)
     connection = pika.BlockingConnection(parameters)
@@ -33,10 +40,16 @@ def enviar_rabbitmq(palabra):
     print(f"Palabra '{palabra}' enviada a RabbitMQ")
     connection.close()
 
+    # Esto es para kafka
+    p = Producer({'bootstrap.servers': bootstrap_servers})
+    p.produce(topic, value=palabra.encode('utf-8'))
+    p.flush()
+
+
 # Generar palabra aleatoria y enviarla a RabbitMQ
 def enviar_palabra_aleatoria():
     palabra = generar_palabra_aleatoria()
-    enviar_rabbitmq(palabra)
+    enviar_servers(palabra)
 
 # Ejecutar el envío de la palabra aleatoria
 enviar_palabra_aleatoria()
