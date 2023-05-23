1b4b90f5ca133444d1eb8e886b2b596856c7e9c1
juanpabloslopez <jpschiaffino@gmail.com>
2023-05-23 15:30:11 -0400

    change the name

--- tarea2/producer.py
+++ tarea2/producer.py
@@ -2,7 +2,9 @@ import random
 import string
 import pika
 from confluent_kafka import Producer
+import json
 import threading
+import time
 
 # Configuración de RabbitMQ
 RABBITMQ_HOST = 'localhost'
@@ -15,42 +17,56 @@ RABBITMQ_QUEUE = 'my_queue'
 bootstrap_servers = 'localhost:9092'
 topic = 'kafkin'
 
-# Generar palabra aleatoria
-def generar_palabra_aleatoria():
+# Generar palabra aleatoria en formato JSON con timestamp
+def generar_palabra_aleatoria(device_id):
     longitud = random.randint(5, 10)
     palabra = ''.join(random.choices(string.ascii_letters, k=longitud))
-    return palabra
+    data = {
+        'timestamp': int(time.time()),
+        'value': {
+            'data': palabra
+        },
+        'device_id': device_id
+    }
+    return json.dumps(data)
 
-# Enviar palabra aleatoria a RabbitMQ
-def enviar_a_rabbitmq(palabra, dispositivo):
+# Enviar palabra aleatoria a los servidores en formato JSON
+def enviar_servers(palabra, device_id):
     credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
     parameters = pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT, credentials=credentials)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
     channel.queue_declare(queue=RABBITMQ_QUEUE)
-    channel.basic_publish(exchange='', routing_key=RABBITMQ_QUEUE, body=f"[Dispositivo {dispositivo}] {palabra}")
-    print(f"[Dispositivo {dispositivo}] Palabra '{palabra}' enviada a RabbitMQ")
+    channel.basic_publish(exchange='', routing_key=RABBITMQ_QUEUE, body=palabra)
+    print(f"Dispositivo {device_id} - Palabra '{palabra}' enviada a RabbitMQ")
     connection.close()
 
-# Enviar palabra aleatoria a Kafka
-def enviar_a_kafka(palabra, dispositivo):
+    # Esto es para Kafka
     p = Producer({'bootstrap.servers': bootstrap_servers})
-    p.produce(topic, value=f"[Dispositivo {dispositivo}] {palabra}".encode('utf-8'))
-    p.flush()
-    print(f"[Dispositivo {dispositivo}] Palabra '{palabra}' enviada a Kafka")
 
-# Función para enviar palabra aleatoria a ambos servidores
-def enviar_palabra_aleatoria(dispositivo):
-    palabra = generar_palabra_aleatoria()
-    enviar_a_rabbitmq(palabra, dispositivo)
-    enviar_a_kafka(palabra, dispositivo)
+    def delivery_report(err, msg):
+        if err is not None:
+            print('Error al enviar mensaje a Kafka:', err)
+        else:
+            print(f"Dispositivo {device_id} - Mensaje enviado a Kafka: {msg.topic()}[{msg.partition()}]")
+
+    p.produce(topic, value=palabra.encode('utf-8'), callback=delivery_report)
+    p.flush()
+
+# Función para simular un dispositivo IoT
+def dispositivo_iot(device_id):
+    while True:
+        palabra = generar_palabra_aleatoria(device_id)
+        enviar_servers(palabra, device_id)
+        time.sleep(random.randint(1, 5))  # Esperar un tiempo aleatorio antes de enviar el siguiente mensaje
 
 # Crear hilos para simular dispositivos IoT
-threads = []
-num_threads = 5  # Número de dispositivos que deseas simular
+num_dispositivos = 5  # Número de dispositivos a simular
 
-for i in range(num_threads):
-    t = threading.Thread(target=enviar_palabra_aleatoria, args=(i+1,))
+threads = []
+
+for i in range(num_dispositivos):
+    t = threading.Thread(target=dispositivo_iot, args=(i,))
     threads.append(t)
     t.start()
 
